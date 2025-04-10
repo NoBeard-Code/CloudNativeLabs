5bd7b177fbb64da0a7ff9faa39d716400561e61e
Ratko Ćosić <ratko_cosic@hotmail.com>
2025-04-10 12:09:38 +0200

    do it

--- CloudNativeLabs.AspireStarter/CloudNativeLabs.AspireStarter.AppHost/Program.cs
+++ CloudNativeLabs.AspireStarter/CloudNativeLabs.AspireStarter.AppHost/Program.cs
@@ -1,10 +1,14 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var cache = builder.AddRedis("cache");
+
 var apiService = builder.AddProject<Projects.CloudNativeLabs_AspireStarter_ApiService>("apiservice");
 
 builder.AddProject<Projects.CloudNativeLabs_AspireStarter_Web>("webfrontend")
     .WithExternalHttpEndpoints()
     .WithReference(apiService)
-    .WaitFor(apiService);
+    .WaitFor(apiService)
+    .WithReference(cache)
+    .WaitFor(cache);
 
 builder.Build().Run();
